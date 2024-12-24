d2c593cf4275d89f6a774fd6106cce5e77ef1bd6
Libberator <madlibberator@gmail.com>
2024-12-23 17:19:41 -0800

    Initial commit

---
+++ AoC/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AoC;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Solutions/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Solutions;
+
+public class Solver
+{
+}
